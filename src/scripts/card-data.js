6bff069cd428ba7af5f07ed6d8d130f1afcacc71
Fernanda Tollotti <fernandatollotti@gmail.com>
2024-11-30 12:00:24 -0300

    feat: create card data module and define card information

---
+++ src/scripts/card-data.js
@@ -0,0 +1,27 @@
+const pathImages = 'src/assets/icons/';
+const cardData = [
+  {
+    id: 0,
+    name: "Blue Eyes White Dragon",
+    type: "Paper",
+    image: `${pathImages}dragon.png`,
+    WinOf: [1],
+    LoseOf: [2]
+  },
+  {
+    id: 1,
+    name: "Dark Macigian",
+    type: "Rock",
+    image: `${pathImages}macigian.png`,
+    WinOf: [2],
+    LoseOf: [0]
+  },
+  {
+    id: 2,
+    name: "Exodia",
+    type: "Scissors",
+    image: `${pathImages}exodia.png`,
+    WinOf: [0],
+    LoseOf: [1]
+  }
+];
