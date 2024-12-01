6c2d55e2ea5124b8607b80b54d1fe4408fcdde50
Fernanda Tollotti <fernandatollotti@gmail.com>
2024-12-01 18:06:24 -0300

    feat: implement random card generation, card drawing and DOM manipulation

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -15,3 +15,40 @@ const state = {
   },
   button: document.getElementById('next-duel')
 }
+
+const playerSides = {
+  player1: "player-cards",
+  computer: "computer-cards"
+}
+
+async function getRandomIdCard() {
+  const randomIndex = Math.floor(Math.random() * cardData.length);
+
+  return cardData[randomIndex].id;
+}
+
+async function createCardImage(idCard, fieldSide) {
+  const cardImage = document.createElement('img');
+  cardImage.setAttribute('height', '100px');
+  cardImage.setAttribute('src', 'src/assets/icons/card-back.png');
+  cardImage.setAttribute('data-id', idCard);
+  cardImage.classList.add('card');
+
+  return cardImage;
+}
+
+async function drawCards(cardNumbers, fieldSide) {
+  for (let i = 0; i < cardNumbers; i++) {
+    const randomIdCard = await getRandomIdCard();
+    const cardImage = await createCardImage(randomIdCard, fieldSide);
+
+    document.getElementById(fieldSide).appendChild(cardImage);
+  }
+}
+
+function init() {
+  drawCards(5, playerSides.player1);
+  drawCards(5, playerSides.computer);
+}
+
+init();
